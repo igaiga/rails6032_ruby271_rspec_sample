d98d764ca65b50b82765f82a1ca70b6b3c25e42f
Kuniaki Igarashi <igaiga@gmail.com>
2020-07-07 13:51:48 +0900

    Add association trait in factories

--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -3,4 +3,11 @@ FactoryBot.define do
     title { "RubyBook" }
     memo { "Good" }
   end
+
+  trait :with_variations do
+    after(:create) do |book|
+#      book.variations.create!(kind: "paper book")
+      book.variations.create!(FactoryBot.build(:variation).attributes)
+    end
+  end
 end
